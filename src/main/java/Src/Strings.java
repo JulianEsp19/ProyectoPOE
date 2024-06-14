9d6d0f00af34dd950faf26721f1c9ea0b923e37f
JulianEsp19 <117664575+JulianEsp19@users.noreply.github.com>
2024-06-14 00:01:33 -0600

    Funcionamiento de Strings.java
    Se creo el archivo Strings.xml y dentro se agregaron instrucciones para poder agregar nuevos textos
    Ademas String.java ya esta en funcionamiento y ya se puede usar para agregar textos en los forms obteniendolos a partir de la clase

--- src/main/java/Src/Strings.java
+++ src/main/java/Src/Strings.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Src;
 
-/**
- *
- * @author Julian
- */
+import java.io.File;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+
+
 public class Strings {
+    
+    private DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    private org.w3c.dom.Document documento = null;
+    private String direccionXml = "src/main/java/Src/Strings.xml";
+    
+    
+    public Strings() {
+        try{
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            documento = (org.w3c.dom.Document) builder.parse(new File(direccionXml));
+            
+            System.out.println("Archivo abierto con exito");
+        }catch(Exception e){
+            System.err.println(e.getClass().getName() + ":" + e.getMessage());
+        }
+    }
+    
+    public String getText(String nombreText){
+        String texto = null;
+        NodeList listaString = documento.getElementsByTagName("String");
+        
+        for (int i = 0; i < listaString.getLength(); i++) {
+            Node nodoString = listaString.item(i);
+            System.out.println(nodoString.getAttributes().getNamedItem("value"));
+            if(nodoString.getAttributes().getNamedItem("value").getTextContent().equals(nombreText)){
+                texto = nodoString.getTextContent();
+            }
+        }
+        
+        return texto;
+    }
+    
     
 }
