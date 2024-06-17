cfb09b2aa1a4d997fd37b2bd64845631805d30b7
deandaf051 <77902558+deandaf051@users.noreply.github.com>
2024-06-17 02:13:36 -0600

    Verificar sesion
    Aun no esta completo, falta verificar si funciona cuando el login sirva.

--- src/main/java/Forms/Main.java
+++ src/main/java/Forms/Main.java
@@ -1,11 +1,38 @@
 package Forms;
-
-
+import Clases.UsuarioLogIn;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 public class Main extends javax.swing.JFrame {
 
     public Main() {
+       
         initComponents();
+        
+        
     }
+    
+   private boolean verificarSesion(){
+    UsuarioLogIn usuariologin = new UsuarioLogIn();
+    try {
+        if (!usuariologin.obtenerSesion()) {
+            JOptionPane.showMessageDialog(this, "No hay una sesión guardada. Redirigiendo al login.");
+            this.dispose();
+            SwingUtilities.invokeLater(() -> {
+                new Login().setVisible(true); // Muestra Login
+            });
+            return false; // Indica que la sesión no es válida
+        }
+    } catch(Exception e){
+        e.printStackTrace();
+        JOptionPane.showMessageDialog(this, "Error al comprobar la sesión.");
+        this.dispose();
+        SwingUtilities.invokeLater(() -> {
+            new Login().setVisible(true); // Muestra Login
+        });
+        return false; // Indica que hubo un error y la sesión no es válida
+    }
+    return true; // Indica que la sesión es válida (creo xd)
+} 
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -27,7 +54,7 @@ public class Main extends javax.swing.JFrame {
 
         jLabel1.setText("jLabel1");
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jPanel1.setForeground(java.awt.Color.white);
 
@@ -145,36 +172,16 @@ public class Main extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
+       
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Main().setVisible(true);
+               
+               Main mainFrame = new Main();
+               
+               if(mainFrame.verificarSesion()){
+                   mainFrame.setVisible(true);
+               }
+                
             }
         });
     }
