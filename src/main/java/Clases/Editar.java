02c507566674c316d6e4167763904fb82c9e6dd3
JulianEsp19 <117664575+JulianEsp19@users.noreply.github.com>
2024-06-17 00:20:51 -0600

    Editar.java funcional para Usuarios
    Ahora se puede editar los Usuarios respecto a dos objetos de tipo Usuario.java uno para el antiguo y otro para el nuevo

--- src/main/java/Clases/Editar.java
+++ src/main/java/Clases/Editar.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Clases;
 
-/**
- *
- * @author Julian
- */
-public class Editar {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Editar extends DataBase{
+
+    public Editar() {
+    }
+    
+    public void editarUsuario(Usuario antiguo, Usuario nuevo) throws SQLException{
+        int rowid = 0;
+        stmt = conexion.createStatement();
+        
+        String busquedaAntiguo = "SELECT ROWID FROM Usuarios WHERE "
+                + "NombreUsuario = '" + antiguo.getUsuario() + "' AND "
+                + "ContrasenaUsuario = '"+ antiguo.getContrasena() +"'";
+        
+        ResultSet resultado = stmt.executeQuery(busquedaAntiguo);
+        
+        rowid = resultado.getInt("rowid");
+        
+        String editarUsuario = "UPDATE Usuarios SET "
+                + "NombreUsuario = '"+nuevo.getUsuario()+"', "
+                + "ContrasenaUsuario = '"+nuevo.getContrasena() + "', "
+                + "TipoAcceso = "+nuevo.getTipoAcceso()+" "
+                + "WHERE ROWID="+ rowid;
+        
+        stmt.executeUpdate(editarUsuario);
+        stmt.close();
+    }
     
 }
