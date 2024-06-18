0c30e1a9143202af2057c22cc738025e1c8240b0
JulianEsp19 <117664575+JulianEsp19@users.noreply.github.com>
2024-06-17 23:33:44 -0600

    Create Atributos.java

---
+++ src/main/java/Src/Atributos.java
@@ -0,0 +1,61 @@
+package Src;
+
+public class Atributos {
+    
+    public String[] Ingresos = {"FechaIngreso", 
+        "Almacen", 
+        "FechaDe_R_OP", 
+        "FechaSalidaCliente", 
+        "ProgramadaCheckList", 
+        "R_OP", 
+        "OrdenCompra", 
+        "OrdenCompraProvedor", 
+        "Modelo", 
+        "TipoEntregar", 
+        "ClaveIdentificador", 
+        "R_OP2", 
+        "Clave", 
+        "Provedor", 
+        "Cliente", 
+        "NumPedidoProvedor", 
+        "ClaveProducto", 
+        "Tarimas", 
+        "PiezaporBulto", 
+        "TotalBultos", 
+        "TotalPiezas", 
+        "PiezasRequendas", 
+        "Paletizado", 
+        "CamaPorPaletA1", 
+        "CamaPorPaletA2", 
+        "EstibasPorPalet", 
+        "TotalPiezasFinal", 
+        "Alto", 
+        "Ancho", 
+        "Largo", 
+        "CalibreFlauta", 
+        "Medidas"
+        };
+    public String[] Salida = {"Lugar", 
+        "NumDeTarimas", 
+        "R_OP", 
+        "Clave", 
+        "Cliente", 
+        "Modelo", 
+        "PiezaPorBulto", 
+        "TotalBultos", 
+        "PiezasExtras", 
+        "TotalPiezas"
+        };
+    public String[] Inventario = {"Lugar", 
+        "NumTarimas", 
+        "Clave", 
+        "Cliente", 
+        "Modelo", 
+        "PzBulto", 
+        "TotalBultos", 
+        "PzExtras", 
+        "TotalPiezas", 
+        "Nota"
+        };
+    
+}
